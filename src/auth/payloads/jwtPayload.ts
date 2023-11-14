d14e05cf320f323df176ca3bb0f0ea4107be01a2
yukioqna <nguyenngocdung19082002@gmail.com>
2023-11-15 04:06:05 +0700

    Add initial data

--- src/auth/payloads/jwtPayload.ts
+++
@@ -1,6 +0,0 @@
-import { Role } from '../../modules/users/roles.enum';
-
-export default interface JwtPayload {
-   email: string;
-   roles: Role[];
-}
--- src/common/interface/custom-request.interface.ts
+++
@@ -1,5 +0,0 @@
-import { User } from '../../modules/users/entities/user.entity';
-
-export interface CustomRequest extends Request {
-   user?: User;
-}
